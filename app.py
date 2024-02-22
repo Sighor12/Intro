095009ad1724587aa39ecdff2121ef2fd5ce8ce8
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-02-22 10:38:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,9 @@ with col2:
     st.write('La audición es fundamental para tu interfaz')
   if modo == 'Táctil':
     st.write('El tacto es fundamental para tu interfaz')
+
+st.subheader("Uso de Botones")
+if st.button('presiona el botón'):
+  st.write('Gracias por presionar')
+else:
+  st.write('No has presionado aún')
